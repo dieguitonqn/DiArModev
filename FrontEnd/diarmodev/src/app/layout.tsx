ec7730ff4677376132ab0c1b88107ee228e24678
Diego Morales <diegoariel.morales@gmail.com>
2025-04-21 23:31:39 -0300

    le agregué el og

--- FrontEnd/diarmodev/src/app/layout.tsx
+++ FrontEnd/diarmodev/src/app/layout.tsx
@@ -19,6 +19,41 @@ export default function RootLayout({
   return (
     <html lang="es">
 
+<head>
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="DiArMo dev" />
+        <meta property="og:locale" content="es-AR" />
+        <meta property="og:url" content="https://mildestinos.tur.ar" />
+        <meta property="og:title" content="DiArMo dev" />
+        <meta
+          property="og:description"
+          content="Desarrollador de soluciones web totalmente a medida, simples y potentes ."
+        />
+        <meta
+          property="og:image"
+          content="https://diarmodev.com/diarmodev_logo_trans.png"
+        />
+        <meta
+          property="og:image:width"
+          content="1599"
+        />
+        <meta
+          property="og:image:height"
+          content="1599"
+        />
+        <meta
+          property="og:image:alt"
+          content="DiArMo dev logo"
+        />
+        <meta
+          property="og:image:type"
+          content="image/png"
+        />
+        
+        
+        
+      </head>
+
       <body className={inter.className}>
         <Navbar />
         {children}
