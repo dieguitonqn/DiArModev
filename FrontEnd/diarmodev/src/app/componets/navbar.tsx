0e7b053a4b6e64c57b4b799ea7e60b1eb2255ab1
Diego Morales <diegoariel.morales@gmail.com>
2025-04-24 15:34:37 -0300

    agregué el hamburguesa

--- FrontEnd/diarmodev/src/app/componets/navbar.tsx
+++ FrontEnd/diarmodev/src/app/componets/navbar.tsx
@@ -1,9 +1,16 @@
 // components/Navbar.js
-
+'use client'
 import Link from 'next/link';
 import Image from 'next/image';
-
+import { useState } from 'react';
+import { SlMenu } from "react-icons/sl";
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <nav className="sticky top-0 bg-gray-950 p-4 shadow-slate-800 shadow-lg border-4 border-black border-b-slate-800">
             <div className="max-w-7xl mx-auto px-4">
@@ -35,15 +42,34 @@ const Navbar = () => {
                                     Acerca de mi
                                 </Link>
                             </li>
-                            {/* <li>
-                                <Link href={"#"}
-                                    className="text-white hover:text-gray-300">
-                                    Contacto
-                                </Link>
-                            </li> */}
                         </ul>
                     </div>
+                    <div className="md:hidden">
+                        <button
+                            onClick={toggleMenu}
+                            className="text-white focus:outline-none">
+                            <SlMenu className='h-8 w-10' />
+                        </button>
+                    </div>
                 </div>
+                {isMenuOpen && (
+                    <div className="md:hidden mt-4 w-full flex justify-end">
+                        <ul className="space-y-2 text-center">
+                            <li>
+                                <Link href={"/"}
+                                    className="block text-white hover:text-gray-300 p-1 hover:shadow-lg shadow-sm shadow-slate-400 border-b border-gray-500 rounded">
+                                    Inicio
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/aboutMe"}
+                                    className="block text-white hover:text-gray-300 p-1 hover:shadow-lg shadow-sm shadow-slate-400 border-b border-gray-500 rounded">
+                                    Acerca de mi
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                )}
             </div>
         </nav>
     );
